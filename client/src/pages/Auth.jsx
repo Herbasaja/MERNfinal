26d61045f8c4634091a7f5559e8f8bd8f0074cde
Herbasaja <45150535+Herbasaja@users.noreply.github.com>
2022-10-23 22:47:18 +0300

    Added navigation to auth prevention for logged in.

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import Login from '../components/auth/Login'
-import Register from '../components/auth/Register'
-import Layout from '../components/Layout'
+import React, { useEffect }from 'react'
+import Login from '../components/auth/Login';
+import Register from '../components/auth/Register';
+import Layout from '../components/Layout';
 import classes from './Auth.module.scss';
+import useAuth from '../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 function Auth() {
+  const { auth } = useAuth()
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (auth){
+      navigate('/');
+    }
+  }, [auth, navigate]);
+
   return (
     <Layout>
       <div className={classes.form_container}>
